2d3710af63ffb1393629eac55e0387d606faa23f
Cillian <G00404411@atu.ie>
2023-10-10 16:36:57 +0100

    In the process of Exercise1

---
+++ src/main/java/ie/atu/week3/ConstructorInjectedController.java
@@ -0,0 +1,10 @@
+package ie.atu.week3;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConstructorInjectedController {
+    private UserService userService;
+
+
+}
---
+++ src/main/java/ie/atu/week3/EmailService.java
@@ -0,0 +1,10 @@
+package ie.atu.week3;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    public void sendEmail(String to, String message){
+        System.out.println("Your name is: " + to + "Your email is: " + message);
+    }
+}
